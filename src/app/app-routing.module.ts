654ef220aaad600517f3a88970147ee2745ef2aa
Dawson Heide <dheide@Dawsons-iMac.local>
2018-08-06 08:44:29 -0400

    routes finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component'
+import { ResearchPageComponent } from './research-page/research-page.component'
+import { ContactPageComponent } from './contact-page/contact-page.component'
+import { ResumePageComponent } from './resume-page/resume-page.component'
+const routes: Routes = [
+	 { path: '', redirectTo: '/home', pathMatch: 'full' },
+	 { path: 'home', component: HomePageComponent },
+	 { path: 'resume', component: ResumePageComponent },
+	 { path: 'contact', component: ContactPageComponent },
+	 { path: 'research', component: ResearchPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ResearchPageComponent } from './research-page/research-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ResumePageComponent } from './resume-page/resume-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    ResearchPageComponent,
+    ContactPageComponent,
+    ResumePageComponent
   ],
   imports: [
     BrowserModule,
